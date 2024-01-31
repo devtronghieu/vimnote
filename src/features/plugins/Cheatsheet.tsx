d19a78875ec7b3f9233f8dc3fdccfec63cba4e6d
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-31 16:01:09 +0700

    ui: update cheatsheet animation

--- src/features/plugins/Cheatsheet.tsx
+++ src/features/plugins/Cheatsheet.tsx
@@ -8,21 +8,18 @@ const Cheatsheet = () => {
     PluginModal.Cheatsheet,
   );
 
-  if (!isShown) {
-    return null;
-  }
-
   return (
     <Modal
       center
       className={`
-        h-4/5 w-3/5 p-8
+        h-4/5 w-[90%] lg:w-3/5 p-8 overflow-scroll no-scrollbar
         glass-effect with-border
+        transition-opacity duration-300 ease-in ${isShown ? "opacity-100" : "opacity-0"}
       `}
     >
       <h2 className="text-xl font-bold text-center mb-6">Cheat sheet</h2>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {Object.values(Keymap).map((mode) => {
           return (
             <div key={mode.name} className="flex flex-1 flex-col gap-4">
