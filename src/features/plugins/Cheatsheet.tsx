368e05507cfa11f8cbcfca35176c33b1951017e5
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-31 17:56:25 +0700

    ui: remove cheatsheet animation

--- src/features/plugins/Cheatsheet.tsx
+++ src/features/plugins/Cheatsheet.tsx
@@ -8,13 +8,14 @@ const Cheatsheet = () => {
     PluginModal.Cheatsheet,
   );
 
+  if (!isShown) return null;
+
   return (
     <Modal
       center
       className={`
         h-4/5 w-[90%] lg:w-3/5 p-8 overflow-scroll no-scrollbar
         glass-effect with-border
-        transition-opacity duration-300 ease-in ${isShown ? "opacity-100" : "opacity-0"}
       `}
     >
       <h2 className="text-xl font-bold text-center mb-6">Cheat sheet</h2>
